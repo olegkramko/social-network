729e3d73eee790023564bdc11d366aad0fedb1b0
OlegKram <kramoleh@gmail.com>
2018-08-20 21:28:46 +0300

    freemaker

--- src/main/java/coding/twitterclone/controller/MainController.java
+++ src/main/java/coding/twitterclone/controller/MainController.java
@@ -6,8 +6,8 @@ import coding.twitterclone.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
 import java.util.Map;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
--- src/main/java/coding/twitterclone/repos/MessageRepo.java
+++ src/main/java/coding/twitterclone/repos/MessageRepo.java
@@ -1,12 +1,9 @@
 package coding.twitterclone.repos;
-
 import coding.twitterclone.domain.Message;
 import org.springframework.data.repository.CrudRepository;
-
 import java.util.List;
 
 public interface MessageRepo extends CrudRepository<Message, Long> {
-
     //messages search
     List<Message> findByTag(String tag);
 }
